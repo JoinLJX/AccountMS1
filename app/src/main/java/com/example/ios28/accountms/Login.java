342e88590be3bb70136b953531766c1d48b38d68
lmy <1376442710@qq.com>
2017-09-14 11:33:35 +0800

    登录，退出，系统设置完善

--- app/src/main/java/com/example/ios28/accountms/Login.java
+++ app/src/main/java/com/example/ios28/accountms/Login.java
@@ -1,19 +1,52 @@
 package com.example.ios28.accountms;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.ios28.accountms.AccountDAO.PwdDAO;
+
+/**
+ * 登录模块
+ */
 public class Login extends AppCompatActivity {
 
+    EditText txtlogin;
+    Button btnlogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-
+        txtlogin = (EditText)findViewById(R.id.txtLogin);
+        btnlogin = (Button)findViewById(R.id.btnLogin);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this,MainActivity.class);
+                PwdDAO pwdDAO = new PwdDAO(Login.this);
+                if((pwdDAO.getCount()==0 || pwdDAO.find().getPassword().isEmpty())&&
+                        txtlogin.getText().toString().isEmpty()) {
+                    startActivity(intent);
+                }
+                else {
+                    if (pwdDAO.find().getPassword().equals(txtlogin.getText().toString())){
+                        startActivity(intent);
+                    }
+                    else {
+                        Toast.makeText(Login.this,"密码错误，请输入正确的密码",Toast.LENGTH_LONG).show();
+                    }
+                }
+                txtlogin.setText("");
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/ios28/accountms/Sysset.java
+++ app/src/main/java/com/example/ios28/accountms/Sysset.java
@@ -6,14 +6,50 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.ios28.accountms.AccountDAO.PwdDAO;
+import com.example.ios28.accountms.model.Tb_pwd;
+
+/**
+ * 系统设置功能模块
+ */
 public class Sysset extends AppCompatActivity {
 
+    private EditText txtpwd;
+    private Button btnSet,btnsetCancel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sysset);
+        txtpwd = (EditText)findViewById(R.id.txtPwd);//获取输入密码的文本框
+        btnSet = (Button)findViewById(R.id.btnSet);//获取设置按钮
+        btnsetCancel = (Button)findViewById(R.id.btnsetCancel);//获取取消按钮
 
+        btnSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PwdDAO pwdDAO = new PwdDAO(Sysset.this);
+                Tb_pwd tb_pwd = new Tb_pwd(txtpwd.getText().toString());
+                if(pwdDAO.getCount()==0){
+                    pwdDAO.add(tb_pwd);
+                }
+                else {
+                    pwdDAO.update(tb_pwd);
+                }
+                Toast.makeText(Sysset.this,"密码设置成功！",Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+        btnsetCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtpwd.setText("");
+                txtpwd.setHint("请输入密码");
+            }
+        });
     }
 
 }
